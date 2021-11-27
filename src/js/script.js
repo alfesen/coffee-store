1cd142d6d7cdbc2e12aef53b04c0f1283097ff21
alfesen <89510380+alfesen@users.noreply.github.com>
2021-11-27 10:15:16 +0100

    initHome

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,32 @@
-// import { settings } from './settings.js';
-// const app = {
-//   initData: function() {
-//     const url = settings.db.url + '/' + settings.db.products;
-//     this.data = {};
-//     fetch(url)
-//       .then((rawResponse) => {
-//         return rawResponse.json();
-//       })
-//       .then((parsedResponse) => {
-//         this.data.products = parsedResponse;
-//       });
-//   },
+import { settings } from './settings.js';
+import Home from './components/Home.js';
 
-//   init: function() {
-//     const thisApp = this;
-//     thisApp.initData();
-//   },
-// };
+const app = {
+  initData: function() {
+    const url = settings.db.url + '/' + settings.db.products;
+    this.data = {};
+    fetch(url)
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((parsedResponse) => {
+        this.data.products = parsedResponse;
+      });
+  },
 
-// app.init();
+  
+  initHome: function() {
+    const thisApp = this;
+    
+    thisApp.home = new Home();
+  },
+  
+  init: function() {
+    const thisApp = this;
+    thisApp.initData();
+    thisApp.initHome();
+  },
+  
+};
+
+app.init();
