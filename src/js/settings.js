5c186a6f02eb0ac795e4a288cf6f2737008160cc
alfesen <89510380+alfesen@users.noreply.github.com>
2021-11-28 13:49:52 +0100

    Prepair references and templates for about

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,9 +1,19 @@
 export const select = {
   containerOf: {
     homePage: '#home-page',
+    about: '#about',
+    aboutUs: '#about-us',
   },
   templateof: {
-    homePage: '#template-home-page'
+    homePage: '#template-home-page',
+    about: '#template-about',
+  },
+  product: {
+    name: '.name',
+    description: '.description',
+    intensity: '.intensity',
+    roasting: '.roasting',
+    image: '.product-image',
   }
 };
 
@@ -16,5 +26,6 @@ export const settings = {
 
 export const templates = {
   homePage: Handlebars.compile(document.querySelector(select.templateof.homePage).innerHTML),
+  about: Handlebars.compile(document.querySelector(select.templateof.about).innerHTML),
 };
 
