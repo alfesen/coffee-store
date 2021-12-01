6a915664554903d57ee09c8504c40737cf9b87bc
alfesen <89510380+alfesen@users.noreply.github.com>
2021-12-01 19:46:49 +0100

    Fetch data for products and about

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ import {
   settings
 } from './settings.js';
 import Product from './components/Product.js';
+import About from './components/About.js';
 
 export const app = {
 
@@ -9,9 +10,9 @@ export const app = {
     const thisApp = this;
 
     thisApp.data = {};
-    const url = settings.db.url + '/' + settings.db.products;
-
-    fetch(url) // fetch(połącz się), zapytanie(request) pod podany adres endpointu(url)
+    const productsUrl = settings.db.url + '/' + settings.db.products;
+    
+    fetch(productsUrl) // fetch(połącz się), zapytanie(request) pod podany adres endpointu(url)
       .then(function (rawResponse) { // odpowiedź w formacie JSON
         return rawResponse.json(); // konwersja odpowiedzi na obiekt JS-owy
       })
@@ -22,27 +23,39 @@ export const app = {
         thisApp.data.products = parsedResponse;
 
 
-        for (let productData in thisApp.data.products) {
-          new Product(thisApp.data.products[productData].id, thisApp.data.products[productData].data);
+        for (let productData in thisApp.data.products[0]) {
+          console.log('data1: ',  thisApp.data.products[0], productData);
+          new Product(thisApp.data.products[0][productData].id, thisApp.data.products[0][productData]);
         }
 
       });
 
+    const aboutUrl = settings.db.url + '/' + settings.db.about;
+    fetch(aboutUrl)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        console.log('response: ', parsedResponse);
+
+        thisApp.data.about = parsedResponse;
+
+        for (let aboutData in thisApp.data.about[0]) {
+          console.log('data2: ', thisApp.data.about[0], aboutData);
+          new About(thisApp.data.about[0][aboutData].id, thisApp.data.about[0][aboutData]);
+        }
+      });
+    
+    console.log('about: ', aboutUrl);
+
+    
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
 
-
-  initProduct: function () {
-    const thisApp = this;
-
-    thisApp.products = new Product();
-  },
-
   init: function () {
     const thisApp = this;
 
-    thisApp.initProduct();
     thisApp.initData();
   },
 
