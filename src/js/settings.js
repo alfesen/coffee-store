6ad83f7574812eedff510f1ad39d0d90da842089
alfesen <89510380+alfesen@users.noreply.github.com>
2021-12-01 19:46:10 +0100

    Add settings and handlebars for about section, remove unnecessary

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,29 +2,23 @@ export const select = {
   containerOf: {
     homePage: '#home-page',
     about: '#about',
-    aboutUs: '#about-us',
   },
   templateof: {
     homePage: '#template-home-page',
     about: '#template-about',
   },
-  product: {
-    name: '.name',
-    description: '.description',
-    intensity: '.intensity',
-    roasting: '.roasting',
-    image: '.product-image',
-  }
 };
 
 export const settings = {
   db: {
     url: '//' + window.location.hostname + (window.location.hostname == 'localhost'? ':3131' : ''),
     products: 'products',
+    about: 'about',
   },
 };
 
 export const templates = {
   homePage: Handlebars.compile(document.querySelector(select.templateof.homePage).innerHTML),
+  aboutSection: Handlebars.compile(document.querySelector(select.templateof.about).innerHTML),
 };
 
