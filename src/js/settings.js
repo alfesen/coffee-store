fc62dc5b7c860814761b7187023c61b54b94b5b4
alfesen <89510380+alfesen@users.noreply.github.com>
2021-12-04 13:38:42 +0100

    Ädd references for sections, buttons and classNames

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,26 @@ export const select = {
   containerOf: {
     homePage: '#home-page',
     about: '#about',
+    pages: '#pages',
   },
   templateof: {
     homePage: '#template-home-page',
     about: '#template-about',
+  },
+  sectionOf: {
+    about: '.about',
+    products: '#products',
+    contact: '#contact',
+  },
+  buttons: {
+    navLinks: '.navbar-nav a',
+    home: '.home-btn',
+    products: '.product-btn',
+    contact: '.contact-btn',
+  },
+  classNames: {
+    active: 'active',
+    post: 'post',
   },
 };
 
